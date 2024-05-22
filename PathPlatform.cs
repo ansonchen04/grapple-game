af04d3e7859f50268ab531057a7fd485fc98d664
TheNarrator42 <43051137+TheNarrator42@users.noreply.github.com>
2024-05-22 17:49:06 -0400

    Added Path with a configurable platform
    Create a new 2d node named MovingPlatformPath that allows the user to choose discrete points for the platform to move upon in a loop

---
+++ PathPlatform.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class PathPlatform : Node2D
+{
+	private PathFollow2D pathFollow;
+	[Export]
+	public int Speed = 500;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		pathFollow = GetNode<PathFollow2D>("Path2D/PathFollow2D");
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		pathFollow.Progress += (float)(Speed*delta);
+	}
+}
