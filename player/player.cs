d37584682fb6afb9c44ee96021d04bc1984083be
TheNarrator42 <43051137+TheNarrator42@users.noreply.github.com>
2024-06-02 00:29:50 -0400

    Revert "Started working on level 1"
    This reverts commit 4c0a2db7c28b41edebbce63f5466f2e51f44b70f.

--- player/player.cs
+++ player/player.cs
@@ -4,7 +4,7 @@ using System;
 public partial class player : CharacterBody2D
 {
 	public const float Speed = 300.0f;
-	public const float JumpVelocity = -600.0f;
+	public const float JumpVelocity = -400.0f;
 
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
 	public float gravity = ProjectSettings.GetSetting("physics/2d/default_gravity").AsSingle();
@@ -18,12 +18,12 @@ public partial class player : CharacterBody2D
 			velocity.Y += gravity * (float)delta;
 
 		// Handle Jump.
-		if (Input.IsActionJustPressed("Up") && IsOnFloor())
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
 			velocity.Y = JumpVelocity;
 
 		// Get the input direction and handle the movement/deceleration.
 		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("Left", "Right", "Up", "Down");
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
 			velocity.X = direction.X * Speed;
@@ -32,12 +32,6 @@ public partial class player : CharacterBody2D
 		{
 			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
 		}
-
-		// Drop the player down 1 pixel if standing on a one-way collision platform and "ui_drop_down" is pressed
-        if (IsOnFloor() && Input.IsActionJustPressed("Down"))
-        {
-            Position += new Vector2(0, 1);
-        }
 
 		Velocity = velocity;
 		MoveAndSlide();
