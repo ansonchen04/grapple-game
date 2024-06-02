4c0a2db7c28b41edebbce63f5466f2e51f44b70f
TheNarrator42 <43051137+TheNarrator42@users.noreply.github.com>
2024-06-02 00:28:20 -0400

    Started working on level 1
    Increased char jump height
    Added template for more varied platforms
    Added template for one way platforms

--- DropDownSprite.cs
+++ DropDownSprite.cs
@@ -1,20 +1,20 @@
 using Godot;
 using System;
 
-public partial class PathPlatform : Node2D
+public partial class DropDownSprite : Polygon2D
 {
-	private PathFollow2D pathFollow;
-	[Export]
-	public int Speed = 500;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		pathFollow = GetNode<PathFollow2D>("PlatformPath/PathFollow2D");
+		var poly = new CollisionPolygon2D();
+        poly.Polygon = this.Polygon;
+        poly.Position = this.Position;
+		poly.OneWayCollision = true;
+        GetParent().CallDeferred("add_child", poly);
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		pathFollow.Progress += (float)(Speed*delta);
 	}
 }
