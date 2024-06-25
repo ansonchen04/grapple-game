ed31fc683626b74b0804cd9a6f3531a412ff677e
TheNarrator42 <43051137+TheNarrator42@users.noreply.github.com>
2024-06-25 17:14:18 -0400

    Cleaned up code
    made minor changes to follow conventional formatting standards

--- player/player.cs
+++ player/player.cs
@@ -8,11 +8,12 @@ public partial class player : CharacterBody2D
 {
 
 	//How fast the player moves and how high they can jump
-	private const float Speed = 300.0f;
-	private const float JumpVelocity = -600.0f;
-	private const float ClimbVelocity = -200.0f;
+	private const float speed = 300.0f;
+	private const float jumpVelocity = -600.0f;
+	private const float climbVelocity = -200.0f;
 	//Starting Position, should be updated whenever player enters a new scene
 	private Vector2 startPosition;
+	//This vector will be deleted in a future commit
 	private Vector2 OOB = new Vector2(4500,2500);
  	private Vector2 hookStartPos;
 	// Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -20,17 +21,15 @@ public partial class player : CharacterBody2D
 	//Ray is in the center of the player model, checking what platform the player is on
 	private RayCast2D _downwardRaycast;
   //TODO Rename raycast length to a clearer name
-  private const float RaycastLength = 105.0f;
-  private RayCast2D rayCast;
-  private bool isGrappled = false;
-  Node2D rope;
+  	private const float raycastLength = 105.0f;
+  	private RayCast2D rayCast;
+  	private bool isGrappled = false;
+  	Node2D rope;
 	Vector2 ropePull;
-	
 	//Booleans to check if we are on a special surface, if we have different movement options
-	bool onClimbableSurface = false;
-	bool onOneWaySurface = false;
-    public override void _Ready()
-    {
+	private bool onClimbableSurface = false;
+	private bool onOneWaySurface = false;
+    public override void _Ready() {
         // Initialize the RayCast2D node
         _downwardRaycast = GetNode<RayCast2D>("DownwardRaycast");
 		startPosition = this.GlobalPosition;
@@ -41,19 +40,16 @@ public partial class player : CharacterBody2D
 		GD.Print(rope);
 		ropePull = Vector2.Zero;
     }
-	public override void _PhysicsProcess(double delta)
-	{
+	public override void _PhysicsProcess(double delta) {
 		//This is shit code and will be replaced once I figure out resources
-		if(Position.X > OOB.X || Position.Y > OOB.Y || Input.IsActionJustPressed("Restart")){
+		if (Position.X > OOB.X || Position.Y > OOB.Y || Input.IsActionJustPressed("Restart")) {
 		this.restart();
 		}
         // Check if the player is on the floor or a specific platform using the raycast
-        if (_downwardRaycast.IsColliding())
-        {
+        if (_downwardRaycast.IsColliding()) {
             var collider = _downwardRaycast.GetCollider();
 			//Likely will have to constrain this more
-            if (collider is Node2D platform)
-            {	
+            if (collider is Node2D platform) {	
 				//Will likely have to safeguard this if we collide with platforms without a collision box. 
 				//Not sure when that would happen though
 				this.setOneWay(this.checkOneway(platform));
@@ -62,14 +58,14 @@ public partial class player : CharacterBody2D
 		//Gets the current velocity
 		Vector2 newVelocity = Velocity;
 		//Checking which movement option, if any, is being used. Will convert this into a switch case in a future commit
-		if(onOneWaySurface){
+		if (onOneWaySurface) {
 			onewaydropMovement(newVelocity);
 			newVelocity = baseMovement(newVelocity);
 		}
-		else if(onClimbableSurface){
+		else if (onClimbableSurface) {
 			newVelocity = climbMovement(newVelocity);
 		}
-		else{
+		else {
 		//If nothing fancy, just use base movement vectors
 		newVelocity = baseMovement(newVelocity);
 		}
@@ -91,7 +87,7 @@ public partial class player : CharacterBody2D
 			
 			Vector2 mousePosition = GetGlobalMousePosition();
 			Vector2 direction = (mousePosition - GlobalPosition).Normalized();
-			direction *= RaycastLength;  // need to rename this later!!!
+			direction *= raycastLength;  // need to rename this later!!!
 
 			hookStartPos = direction;
 			rope.Call("SetMouseLoc", direction);
@@ -111,83 +107,75 @@ public partial class player : CharacterBody2D
 			*/
 		}
 	}
-	private Vector2 baseMovement(Vector2 velocity)
-	{
+	private Vector2 baseMovement(Vector2 velocity) {
 		// Get the input direction and handle the movement/deceleration.
 		Vector2 direction = Input.GetVector("Left", "Right", "Up", "Down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
+		if (direction != Vector2.Zero) {
+			velocity.X = direction.X * speed;
 		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		else {
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
 		}
 		// Handle Jump.
 		if (Input.IsActionJustPressed("Up") && IsOnFloor())
-			velocity.Y = JumpVelocity;
+			velocity.Y = jumpVelocity;
 		return velocity;
 	}
-	private Vector2 climbMovement(Vector2 velocity){
+	private Vector2 climbMovement(Vector2 velocity) {
 		// Get the input direction and handle the movement/deceleration.
 		Vector2 direction = Input.GetVector("Left", "Right", "Up", "Down");
-		if (direction != Vector2.Zero)
-		{
-			velocity.X = direction.X * Speed;
+		if (direction != Vector2.Zero) {
+			velocity.X = direction.X * speed;
 		}
-		else
-		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+		else {
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, speed);
 		}
 		// Handle Jump.
 		if (Input.IsActionPressed("Up"))
-			velocity.Y = ClimbVelocity;
+			velocity.Y = climbVelocity;
 		else if (Input.IsActionPressed("Down"))
-			velocity.Y = -ClimbVelocity;
+			velocity.Y = -climbVelocity;
 		else
 			velocity.Y = 0;
 		return velocity;
 	}
-	private Vector2 onewaydropMovement(Vector2 velocity){
+	private Vector2 onewaydropMovement(Vector2 velocity) {
 		// Drop the player down 1 pixel if standing on a one-way collision platform and "ui_drop_down" is pressed
         //TODO verify this actually does ^
-		if (IsOnFloor() && Input.IsActionJustPressed("Down"))
-        {
+		if (IsOnFloor() && Input.IsActionJustPressed("Down")) {
             Position += new Vector2(0, 1);
         }
 		return velocity;
 	}
-	private Boolean checkOneway(Node2D platform){
+	private Boolean checkOneway(Node2D platform) {
 		//Gets the collision polygon or collision shape of the platform
 		Node[] children = platform.FindChildren("*","CollisionPolygon2D",false,false).ToArray();
-		if(children.Length == 0){
+		if (children.Length == 0) {
 			children = platform.FindChildren("*","CollisionShape2D",true,false).ToArray();
 			CollisionShape2D collisionPolygon = (CollisionShape2D)children[0];
-			if (collisionPolygon != null && collisionPolygon.OneWayCollision == true)
-            {
+			if (collisionPolygon != null && collisionPolygon.OneWayCollision == true) {
 				return true;
 			}
-			}
-		else{
+		}
+		else {
 			CollisionPolygon2D collisionPolygon = (CollisionPolygon2D)children[0];
-				if (collisionPolygon != null)
-				{
-					if(collisionPolygon.OneWayCollision == true){
+				if (collisionPolygon != null) {
+					if (collisionPolygon.OneWayCollision == true) {
 						return true;
 					}
 				}
-				}
+		}
 		return false;
 	}
-	public void restart(){
+	public void restart() {
 		//If restart button pressed, reset the positon to the start and zero out the velocity
 			Position = startPosition;
 			Velocity = Vector2.Zero;
 	}
-	public void setClimbing(bool onClimbableSurface){
+	public void setClimbing(bool onClimbableSurface) {
         this.onClimbableSurface = onClimbableSurface;
 	}
-	public void setOneWay(bool onOneWaySurface){
+	public void setOneWay(bool onOneWaySurface) {
 		this.onOneWaySurface = onOneWaySurface;
 	}
   // sets the force the rope should be applying to the player
