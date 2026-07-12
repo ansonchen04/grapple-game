cd951ba9beda4c96058ceb9ea1f7cad45d6dc1b9
Anson <ansonchen04@gmail.com>
2026-07-11 23:59:28 -0400

    feat: enable Level 2 button in level select menu

--- menu/LevelSelect.cs
+++ menu/LevelSelect.cs
@@ -5,15 +5,22 @@ public partial class LevelSelect : Control
     public override void _Ready()
     {
         Button level1Button = GetNode<Button>("MenuContainer/Level1Button");
+        Button level2Button = GetNode<Button>("MenuContainer/Level2Button");
         Button backButton = GetNode<Button>("MenuContainer/BackButton");
 
         level1Button.Pressed += OnLevel1Pressed;
+        level2Button.Pressed += OnLevel2Pressed;
         backButton.Pressed += OnBackPressed;
     }
 
     private void OnLevel1Pressed()
     {
         GetTree().ChangeSceneToFile("res://level/level_1/level1.tscn");
+    }
+
+    private void OnLevel2Pressed()
+    {
+        GetTree().ChangeSceneToFile("res://level/level_2/level2.tscn");
     }
 
     private void OnBackPressed()
