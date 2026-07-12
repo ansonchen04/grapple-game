ceac3ba2822f1742941d09b804d9389aa90315d6
Anson <ansonchen04@gmail.com>
2026-07-12 00:10:37 -0400

    feat: implement next level button functionality in level complete menu

--- menu/LevelComplete.cs
+++ menu/LevelComplete.cs
@@ -2,13 +2,31 @@ using Godot;
 
 public partial class LevelComplete : Control
 {
+    private Button nextLevelButton;
+
     public override void _Ready()
     {
         Button menuButton = GetNode<Button>("MenuContainer/MenuButton");
         Button replayButton = GetNode<Button>("MenuContainer/ReplayButton");
+        nextLevelButton = GetNode<Button>("MenuContainer/NextLevelButton");
 
         menuButton.Pressed += OnMenuPressed;
         replayButton.Pressed += OnReplayPressed;
+        nextLevelButton.Pressed += OnNextLevelPressed;
+
+        // Determine if there is a next level
+        string currentLevel = player.CurrentLevelPath;
+        if (currentLevel.Contains("level_1"))
+        {
+            nextLevelButton.Disabled = false;
+            nextLevelButton.Text = "Level 2";
+        }
+        else
+        {
+            // No more levels for now
+            nextLevelButton.Disabled = true;
+            nextLevelButton.Text = "No More Levels";
+        }
     }
 
     private void OnMenuPressed()
@@ -28,6 +46,19 @@ public partial class LevelComplete : Control
         {
             GD.PrintErr("LevelComplete: No level path found, falling back to Level 1");
             GetTree().ChangeSceneToFile("res://level/level_1/level1.tscn");
+        }
+    }
+
+    private void OnNextLevelPressed()
+    {
+        string currentLevel = player.CurrentLevelPath;
+        if (currentLevel.Contains("level_1"))
+        {
+            GetTree().ChangeSceneToFile("res://level/level_2/level2.tscn");
+        }
+        else
+        {
+            GD.Print("No more levels available.");
         }
     }
 }
