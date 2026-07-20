0a70c010331ba7abd7de9ca0fa78a088f36c617f
Anson <ansonchen04@gmail.com>
2026-07-20 18:32:43 -0400

    fix: replace GetViewportRect with GetViewport().Size in BackgroundScaler.cs

--- scripts/BackgroundScaler.cs
+++ scripts/BackgroundScaler.cs
@@ -29,7 +29,7 @@ public partial class BackgroundScaler : CanvasLayer
 
     private void UpdateBackgroundScale()
     {
-        var viewportSize = GetViewportRect().Size;
+        var viewportSize = GetViewport().Size;
         float scaleX = viewportSize.X / baseWidth;
         float scaleY = viewportSize.Y / baseHeight;
         
