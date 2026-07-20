a47edc2dab69324ab779797c99bd1a141b671d19
Anson <ansonchen04@gmail.com>
2026-07-20 18:33:35 -0400

    fix: use GetViewportRect().Size to fix Viewport size access error

--- scripts/BackgroundScaler.cs
+++ scripts/BackgroundScaler.cs
@@ -29,7 +29,7 @@ public partial class BackgroundScaler : CanvasLayer
 
     private void UpdateBackgroundScale()
     {
-        var viewportSize = GetViewport().Size;
+        var viewportSize = GetViewportRect().Size;
         float scaleX = viewportSize.X / baseWidth;
         float scaleY = viewportSize.Y / baseHeight;
         
